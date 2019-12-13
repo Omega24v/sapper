6a86573ed11e5f36f974c2a5e5384efde5a3112d
Anton <omega24v1993@gmail.com>
2019-12-13 02:05:38 +0200

    remove build folder from gitignore

---
+++ build/precache-manifest.a1b2c69aefa7aac02fdf8a594c98ec03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca43d42664479f58fc70b8ec5039b99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17edc2fe17e1878faa67",
+    "url": "./static/css/main.86759fc3.chunk.css"
+  },
+  {
+    "revision": "e3cdcbd5416b4ec7a3ca",
+    "url": "./static/js/2.befc57a1.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/2.befc57a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17edc2fe17e1878faa67",
+    "url": "./static/js/main.9b38d80c.chunk.js"
+  },
+  {
+    "revision": "e71567cf15abf80686aa",
+    "url": "./static/js/runtime-main.80e7e293.js"
+  }
+]);
